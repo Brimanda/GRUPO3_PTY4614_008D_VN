19a4490998d0513948e9626e0b7550b4682dbf00
AbrahamEsquenazi6 <ab.esquenazi@duocuc.cl>
2024-12-05 17:32:50 -0300

    Delete Fase3/Evidencias Proyecto/Evidencias de Sistema/Aplicacion/SportRent Finalizado directory

--- Fase3/Evidencias Proyecto/Evidencias de Sistema/Aplicacion/SportRent Finalizado/src/app/components/auth-checker.tsx
+++
@@ -1,6 +0,0 @@
-import { supabase } from "@/app/lib/supabase";
-
-export async function AuthChecker() {
-  const { data: { session } } = await supabase.auth.getSession();
-  return !!session; 
-}
--- Fase3/Evidencias Proyecto/Evidencias de Sistema/Aplicacion/SportRent Finalizado/src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { DashboardPage } from "@/components/component/dashboard/dashboard-page";
-
-export default function Dashboard() {
-
-  return (
-      <DashboardPage />
-  );
-}
